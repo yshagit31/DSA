937b2795d40717177633806cf8bb9d2d39b49376
yshagit31 <yshashank024@gmail.com>
2024-09-29 02:26:15 +0530

    TT

--- Trees/Basic/BinaryTressRep.cpp
+++ Trees/Basic/BinaryTressRep.cpp
@@ -28,4 +28,35 @@
 
 // 1<=vec[i]<=100
 
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node {
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+struct node *newNode(int data) {
+  struct node *node = (struct node *)malloc(sizeof(struct node));
+
+  node->data = data;
+
+  node->left = NULL;
+  node->right = NULL;
+  return (node);
+}
+
+// soln 1
+
+    void create_tree(node* root, vector<int> &vec){
+        root->data=vec[0];
+        root->left=newNode(vec[1]);
+        root->right=newNode(vec[2]);
+        root->left->left=newNode(vec[3]);
+        root->left->right=newNode(vec[4]);
+        root->right->left=newNode(vec[5]);
+        root->right->right=newNode(vec[6]);
+    }
+
  
