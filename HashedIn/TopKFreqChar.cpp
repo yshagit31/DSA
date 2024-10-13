4b1376a568ac02fbda76de55fc92c1a67fca7d32
yshagit31 <yshashank024@gmail.com>
2024-10-13 19:57:24 +0530

    #d5

--- HashedIn/TopKFreqChar.cpp
+++ HashedIn/TopKFreqChar.cpp
@@ -10,7 +10,6 @@
 // Input: nums = [1], k = 1
 // Output: [1]
  
-
 // Constraints:
 
 // 1 <= nums.length <= 105
@@ -18,8 +17,11 @@
 // k is in the range [1, the number of unique elements in the array].
 // It is guaranteed that the answer is unique.
  
-
 // Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+//to basically krna kya hai ki humko top frequent numbers dene hai. To agar hum numbers ki freq priority queue me dale to ho jayega. But freq nikalne ke liye
+// hame cahiye kuch to humne map le liya. Phir ab freq compare to kr rahe pr us freq se related key push krna hai isliye pq me freq ke sath key bhi store kr rahe
+// Aur pq me jo pehle hota hai uske acc sorting hoti hai
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -35,10 +37,14 @@ public:
        {
         hash[n]++;
        } 
+
+       // Traversing hashmap
         for(const auto& pair :hash)
         {
+            // Putting freq in first then key
             pq.push({pair.second,pair.first});
         }
+
         for(int i=0;i<k;i++)
         {
          res.push_back(pq.top().second);
