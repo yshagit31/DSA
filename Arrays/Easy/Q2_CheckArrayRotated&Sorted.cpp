c35f82543819c08ee6fa0dbf2c097ece112eab56
yshagit31 <yshashank024@gmail.com>
2024-10-21 21:31:47 +0530

    A1

--- Arrays/Easy/Q2_CheckArrayRotated&Sorted.cpp
+++ Arrays/Easy/Q2_CheckArrayRotated&Sorted.cpp
@@ -22,14 +22,10 @@
 // Explanation: [1,2,3] is the original sorted array.
 // You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
  
-
 // Constraints:
 
 // 1 <= nums.length <= 100
 // 1 <= nums[i] <= 100
-
-
-
 
 
 #include<bits/stdc++.h>
