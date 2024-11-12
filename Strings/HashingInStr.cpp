cb0e25911662271d5fde3cd63071b6a6064fbb25
yshagit31 <yshashank024@gmail.com>
2024-11-12 07:01:21 +0530

    Str2

---
+++ Strings/HashingInStr.cpp
@@ -0,0 +1,32 @@
+// link - https://www.geeksforgeeks.org/problems/index-of-the-first-occurrence-of-pattern-in-a-text/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=index-of-the-first-occurrence-of-pattern-in-a-text%2F
+
+// Index of the First Occurrence of pattern in a text
+// Difficulty: MediumAccuracy: 50.08%Submissions: 13K+Points: 4
+// Given two strings text and pattern, find the first index where pattern exactly matches with any substring of text. 
+
+// Return -1 if pattern doesn't exist as a substring in text.
+
+// Example 1:
+
+// Input:
+// text = gffgfg
+// pattern = gfg
+// Output: 3
+// Explanation:  Considering 0-based indexing, substring of text from 3rd to last index is gfg.
+// Example 2:
+
+// Input:
+// text = gffggg
+// pattern = gfg
+// Output: -1
+// Explanation: pattern "gfg" does not exist in the text.
+// Your Task:
+// You don't need to read input or print anything. Your task is to complete the function findMatching() which takes the strings text and pattern as the input parameters and returns the first index of matching.
+
+// Expected Time Complexity: O(|text| * |pattern|)
+// Expected Auxiliary Space: O(1)
+
+// Constraints:
+// 1 ≤ |text|, |pattern| ≤  103
+ 
+
