1f3a11c7623f0c85ed204bf56a11c93e019759a6
yshagit31 <yshashank024@gmail.com>
2025-03-19 23:46:44 +0530

    POTD

---
+++ POTD/17-3-2025Divide Array Into Equal Pairs.cpp
@@ -0,0 +1,57 @@
+// 2206. Divide Array Into Equal Pairs
+
+// You are given an integer array nums consisting of 2 * n integers.
+
+// You need to divide nums into n pairs such that:
+
+// Each element belongs to exactly one pair.
+// The elements present in a pair are equal.
+// Return true if nums can be divided into n pairs, otherwise return false.
+
+// Example 1:
+
+// Input: nums = [3,2,3,2,2,2]
+// Output: true
+// Explanation: 
+// There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
+// If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy all the conditions.
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Explanation: 
+// There is no way to divide nums into 4 / 2 = 2 pairs such that the pairs satisfy every condition.
+ 
+
+// Constraints:
+
+// nums.length == 2 * n
+// 1 <= n <= 500
+// 1 <= nums[i] <= 500
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        bool divideArray(vector<int>& nums) {
+            unordered_map <int,int> hash;
+            int cnt=0,n=nums.size();
+            for(int i=0;i<nums.size();i++)
+            {
+                hash[nums[i]]++;
+            }
+            for(const auto &pair:hash)
+            {
+                if(((pair.second)%2) !=0)
+                {
+                    return false;
+                }
+                else{
+                    cnt+=pair.second/2;
+                }
+            }
+            if(cnt!=(n/2)) return false;
+            return true;
+        }
+    };
